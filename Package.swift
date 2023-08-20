89efb5503d669a9e07ecd92e6cbe8caa9c756490
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2023-08-19 23:42:01 -0700

    Start package

--- Package.swift
+++ Package.swift
@@ -4,20 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "HAWebsocketKit",
+    name: "HAWebSocketKit",
+    platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "HAWebsocketKit",
-            targets: ["HAWebsocketKit"]),
+            name: "HAWebSocketKit",
+            targets: ["HAWebSocketKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.6")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HAWebsocketKit"),
+            name: "HAWebSocketKit",
+            dependencies: ["Starscream"]
+        ),
         .testTarget(
-            name: "HAWebsocketKitTests",
-            dependencies: ["HAWebsocketKit"]),
+            name: "HAWebSocketKitTests",
+            dependencies: ["HAWebSocketKit"]),
     ]
 )
--- Sources/HAWebsocketKit/HAWebsocketKit.swift
+++ Sources/HAWebsocketKit/HAWebsocketKit.swift
@@ -1,2 +1,59 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import Starscream
+import OSLog
+
+public class HAWebSocketConnection: WebSocketDelegate {
+    let logger = Logger(subsystem: "HAWebSocketKit", category: "HAWebSocketConnection")
+
+    var socket: WebSocket
+    var isConnected = false
+
+    public init(url serverUrl: URL) {
+        let request = URLRequest(url: serverUrl)
+
+        socket = WebSocket(request: request)
+        socket.delegate = self
+    }
+
+    public func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocketClient) {
+        switch event {
+        case .connected(let headers):
+            isConnected = true
+            print("websocket is connected: \(headers)")
+        case .disconnected(let reason, let code):
+            isConnected = false
+            print("websocket is disconnected: \(reason) with code: \(code)")
+        case .text(let string):
+            print("Received text: \(string)")
+        case .binary(let data):
+            print("Received data: \(data.count)")
+        case .ping(_):
+            break
+        case .pong(_):
+            break
+        case .viabilityChanged(_):
+            break
+        case .reconnectSuggested(_):
+            break
+        case .cancelled:
+            isConnected = false
+        case .error(let error):
+            isConnected = false
+            handleError(error)
+        case .peerClosed:
+            break
+        }
+    }
+
+    func handleError(_ error: Error?) {
+        if let e = error as? WSError {
+            print("websocket encountered an error: \(e.message)")
+            logger.critical("HA websocket encountered an error: \(e.message)")
+        } else if let e = error {
+            logger.critical("HA websocket encountered an error: \(e.localizedDescription)")
+        } else {
+            print("websocket encountered an error")
+        }
+    }
+
+}
